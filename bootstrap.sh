1f81d10f88c22383646dd01f7c3c6a702c84d739
Jinming Hu <438600480@qq.com>
2020-04-21 19:52:45 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,35 @@
-#!/usr/bin/env bash
-
-cd "$(dirname "${BASH_SOURCE}")";
-
-git pull origin master;
-
-function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
-	source ~/.bash_profile;
+backup() {
+  target=$1
+  if [ -e "$target" ]; then           # Does the config file already exist?
+    if [ ! -L "$target" ]; then       # as a pure file?
+      mv "$target" "$target.backup"   # Then backup it
+      echo "-----> Moved your old $target config file to $target.backup"
+    fi
+  fi
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+#!/bin/zsh
+for name in *; do
+  if [ ! -d "$name" ]; then
+    target="$HOME/.$name"
+    if [[ ! "$name" =~ '\.sh$' ]] && [ "$name" != 'README.md' ] && [[ ! "$name" =~ '\.sublime-settings$' ]]; then
+      backup $target
+
+      if [ ! -e "$target" ]; then
+        echo "-----> Symlinking your new $target"
+        ln -s "$PWD/$name" "$target"
+      fi
+    fi
+  fi
+done
+
+REGULAR="\\033[0;39m"
+YELLOW="\\033[1;33m"
+GREEN="\\033[1;32m"
+
+setopt nocasematch
+
+
+zsh ~/.zshrc
+
+echo "Carry on with git setup!"
